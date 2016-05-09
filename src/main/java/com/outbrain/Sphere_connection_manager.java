bccf4b8256b4b833445d47704fff3ab7e29e96ef
Tal BUssel <talbussel@gmail.com>
2016-05-09 16:43:24 +0300

    compare our results with sphere content against api.api

--- src/main/java/com/outbrain/Sphere_connection_manager.java
+++ src/main/java/com/outbrain/Sphere_connection_manager.java
@@ -9,7 +9,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * Created by ilya on 4/27/2016. ssssssss
+ * Created by ilya on 4/27/2016. s
  */
 public class Sphere_connection_manager {
     // query sphere api and returns result
