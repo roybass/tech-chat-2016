c37a15e2ca7e8949bd88614ff380cf46da1d4cfe
ilya <brodetaky.ilya@gmail.com>
2016-05-16 15:09:25 +0300

    add a char bot for command line

---
+++ src/main/java/com/outbrain/chat_bot.java
@@ -0,0 +1,24 @@
+package com.outbrain;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/**
+ * Created by ilya on 5/16/2016.
+ */
+public class chat_bot {
+    public static void main(String[] args) {
+        while(true){
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("Enter String");
+            try{
+                String message = br.readLine();
+                String result = Chat_runner_production.get_Sphere_content(message);
+                System.out.println(result);
+            }catch (Exception ex){
+
+            }
+
+        }
+    }
+}
